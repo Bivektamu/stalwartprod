ecef45565c0ce8a2e2612daf0ef92e0256ed5691
Bivek Gurung <bivek.tamu@gmail.com>
2023-12-02 19:13:04 +1100

    Updating content. Still remains

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,48 +5,67 @@ import { RiComputerFill } from 'react-icons/ri'
 export const Hero = () => {
   return (
     <section id="hero" className='active'>
-        <div id="landing" className='w-full h-screen relative'>
-          <img alt="" src='/images/hero.jpg' className='absolute top-0 left-0 w-full h-full object-cover' />
-          <h1 className="absolute top-80 left-0 right-0 text-center text-black">We're creatives</h1>
-        </div>
+      <div id="landing" className='w-full h-screen relative'>
+        <img alt="" src='/images/hero.jpg' className='absolute top-0 left-0 w-full h-full object-cover' />
 
-        <div className="w-full px-10 border-b-2">
-          <div className="max-w-3xl m-auto py-40 ">
-            <h2 className='mb-4 wow fadeInUp'>From Zero to Hero</h2>
-            <h3 className='mb-10 wow fadeInUp'>BONE EXPANDS YOUR POSSIBILITIES</h3>
-            <p className='wow fadeInUp'>
-              Sed lacus augue, congue vel dapibus at, convallis semper lorem. Etiam volutpat, est eget feugiat tristique, erat leo ullamcorper turpis, vel auctor nunc diam vitae urna. Aliquam quis lacus et augue bibendum tincidunt. Duis id cursus ex. Sed cursus tortor id vehicula finibus. Nulla in pharetra lacus. Nulla nec elit nec sem feugiat eleifend sed sed neque. Mauris consequat in purus id cursus. Nunc suscipit lacinia ante, sit amet pulvinar orci venenatis a.
+        <div className="absolute top-80 left-0 right-0 text-center ">
+          <h1 className="text-black mb-4">We're Creatives</h1>
+          <span className="text-black">Experience the power of our real estate video productions that showcase properties in a unique and compelling way.</span>
+
+        </div>
+        {/* <h1 className="absolute top-80 left-0 right-0 text-center text-black">Captivating Real Estate<br />Video Productions <br />That stand&nbsp;Out</h1> */}
+      </div>
+
+      <div className="w-full px-10 border-b-2">
+        <div className="max-w-3xl m-auto py-40 ">
+          <h2 className='mb-4 wow fadeInUp text-center'>Revolutionizing Real Estate<br />Video Production</h2>
+          <p className='wow fadeInUp text-center mb-4'>
+            At Stalwartprod, we take a unique approach to real estate video production. Our team of experts combines creativity and technical expertise to create captivating videos that showcase the true essence of each property. With our innovative techniques, we bring real estate listings to life, helping our clients stand out in a competitive market.
+          </p>
+
+          <p className='wow fadeInUp text-center'>
+            Capture the attention of potential buyers with high-quality videos that showcase the unique features and beauty of your properties. Our professional video production services will help you stand out from the competition and attract more interested buyers.
+          </p>
+        </div>
+      </div>
+
+      <div className="w-full ">
+        <div className="lg:container px-10 py-40 m-auto  pt-10 ">
+
+          <div className="max-w-3xl m-auto">
+            <h2 className='mb-4 text-center wow fadeInUp'>Our Services</h2>
+            <p className="mb-10 fadeInup wow text-center">
+              At Stalwartprod, we specialize in providing high-quality video production services tailored specifically for real estate clients. Our team of experts utilizes innovative techniques to create compelling videos that effectively showcase properties and attract potential buyers.
             </p>
           </div>
-        </div>
 
-        <div className="w-full ">
-          <div className="lg:container px-10 py-40 m-auto grid grid-cols-1 gap-y-14 md:grid-cols-2 lg:grid-cols-4 pt-10 grid gap-x-10">
+          <div className="w-full grid-cols-1 gap-y-14 md:grid-cols-2 lg:grid-cols-4 grid gap-x-10">
             <div className='text-center'>
               <AiFillBulb className='m-auto mb-6 text-2xl wow fadeInUp' />
-              <h3 className='wow fadeInUp mb-3'>Concept</h3>
-              <p className='wow fadeInUp'>We bring ideas to live. Lorem ipsum dolor sit amet</p>
+              <h3 className='wow fadeInUp mb-3'>Customized Video Solutions for Real Estate</h3>
+              <p className='wow fadeInUp'>We offer a range of customized video solutions designed to meet the unique needs of real estate&nbsp;professionals.</p>
             </div>
 
             <div className='text-center'>
               <AiFillCamera data-offset='0.75' className='m-auto mb-6 text-2xl wow fadeInUp' />
-              <h3 data-offset='0.75' className='wow fadeInUp mb-3'>Photography</h3>
-              <p data-offset='0.75' className='wow fadeInUp'>We bring ideas to live. Lorem ipsum dolor sit amet</p>
+              <h3 data-offset='0.75' className='wow fadeInUp mb-3'>Professional Video Editing&nbsp;Services</h3>
+              <p data-offset='0.75' className='wow fadeInUp'>Our team of skilled editors ensures that your real estate videos are polished and visually&nbsp;appealing.</p>
             </div>
 
             <div className='text-center'>
               <RiComputerFill data-offset='0.7' className='m-auto mb-6 text-2xl wow fadeInUp' />
-              <h3 data-offset='0.7' className='wow fadeInUp mb-3'>Design</h3>
-              <p data-offset='0.7' className='wow fadeInUp'>We bring ideas to live. Lorem ipsum dolor sit amet</p>
+              <h3 data-offset='0.7' className='wow fadeInUp mb-3'>Drone Videography for Real&nbsp;Estate</h3>
+              <p data-offset='0.7' className='wow fadeInUp'>Capture stunning aerial footage of your properties with our professional drone videography&nbsp;services.</p>
             </div>
 
             <div className='text-center'>
               <AiFillCamera data-offset='0.65' className='m-auto mb-6 text-2xl wow fadeInUp' />
-              <h3 data-offset='0.65' className='wow fadeInUp mb-3'>Great Food</h3>
-              <p data-offset='0.65' className='wow fadeInUp'>We bring ideas to live. Lorem ipsum dolor sit amet</p>
+              <h3 data-offset='0.65' className='wow fadeInUp mb-3'>Virtual Tours and<br />360°&nbsp;Videos</h3>
+              <p data-offset='0.65' className='wow fadeInUp'>Immerse potential buyers in virtual tours and 360° videos to showcase properties like never&nbsp;before.</p>
             </div>
           </div>
         </div>
-      </section>
+      </div>
+    </section>
   )
 }
